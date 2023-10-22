00195fd53d65dc214ba165116760c666f18a82b1
LionX-S <13141096848@163.com>
2023-10-22 22:28:19 +0800

    feat:完成publish功能

--- scripts/installModules.js
+++ scripts/installModules.js
@@ -1,2 +1,8 @@
 const fs = require("fs");
 const path = require("path");
+
+// 每次执行下载脚本时，在项目根目录生成一个.npmrc文件，设置针对当前项目的registry地址
+const npmrcPath = path.resolve(__dirname, "../.npmrc");
+if (!fs.existsSync(npmrcPath)) {
+	fs.mkdirSync(npmrcPath);
+}
--- scripts/publishModules.js
+++ scripts/publishModules.js
@@ -3,35 +3,28 @@ const path = require("path");
 const { exec } = require("child_process");
 const { directoryName, registry, version } = require("./config.json");
 
+const tgzModule = path.resolve(__dirname, `../${directoryName}`);
 let promises = [];
 
 // 判断压缩目的文件是否存在
-if (!fs.existsSync(path.resolve(__dirname, `../${directoryName}`))) {
-	fs.mkdirSync(path.resolve(__dirname, `../${directoryName}`));
+if (!fs.existsSync(tgzModule)) {
+	fs.mkdirSync(tgzModule);
 	// 同时创建package.json文件
-	exec(
-		`npm init -y`,
-		{ cwd: path.resolve(__dirname, `../${directoryName}`) },
-		(err) => {
-			if (err) throw err;
-			const packageJson = require(`../${directoryName}/package.json`);
-			packageJson.version = version;
-			fs.writeFileSync(
-				path.resolve(__dirname, `../${directoryName}/package.json`),
-				JSON.stringify(packageJson),
-				"utf-8"
-			);
-
-			console.log(
-				"🚀 ~ file: publishModules.js:19 ~ packageJson:",
-				packageJson
-			);
-		}
-	);
+	exec(`npm init -y`, { cwd: tgzModule }, (err) => {
+		if (err) throw err;
+		const packageJson = require(`../${directoryName}/package.json`);
+		packageJson.version = version;
+		fs.writeFileSync(
+			path.resolve(__dirname, `../${directoryName}/package.json`),
+			JSON.stringify(packageJson),
+			"utf-8"
+		);
+	});
 }
 
 const files = fs.readdirSync(path.resolve(__dirname, "../node_modules"));
 
+// 遍历，采用shell命令压缩文件
 files.forEach((file) => {
 	const filePath = path.join(path.resolve(__dirname, "../node_modules"), file);
 	const stat = fs.statSync(filePath);
@@ -58,26 +51,72 @@ files.forEach((file) => {
 });
 
 Promise.all(promises)
-	.then((results) => {
+	.then(() => {
 		console.log("全部压缩完毕，开始发布");
+		return publishPackage();
+	})
+	.then((message) => {
+		console.log(message);
+		// 成功后删除压缩后的文件
+		console.log("开始删除多余文件夹");
+		deleteFolderRecursive(tgzModule);
+		console.log(`删除完毕`);
+	})
+	.catch(({ err }) => {
+		console.log("失败", err);
+	});
+
+// 发布package方法
+const publishPackage = () => {
+	return new Promise((resolve, reject) => {
+		// 先执行unpublish
 		exec(
-			`npm publish --registry=${registry}`,
-			{ cwd: path.resolve(__dirname, `../${directoryName}`) },
+			`npm unpublish ${directoryName}@${version} --force --registry ${registry}`,
+			{ cwd: tgzModule },
 			(err) => {
+				// 如果报错，则说明版本不存在，开始发布
 				if (err) {
-					console.log("🤪当前版本存在，正在撤销已发布版本", err);
-					exec(
-						`npm unpublish ${directoryName}@${version} --registry ${registry}`,
-						{ cwd: path.resolve(__dirname, `../${directoryName}`) },
-						(err) => {
-							if (err) throw err;
-							console.log("cheban");
-						}
-					);
+					console.log("版本已存在，先unpublish当前版本");
+					commonPublish(resolve, reject);
+				} else {
+					// 成功的话继续发布
+					commonPublish(resolve, reject);
 				}
 			}
 		);
-	})
-	.catch((err) => {
-		console.log("压缩失败", err);
 	});
+};
+
+const commonPublish = (resolve, reject) => {
+	exec(
+		`npm publish --registry=${registry}`,
+		{ cwd: path.resolve(__dirname, `../${directoryName}`) },
+		(err) => {
+			if (err) {
+				console.log();
+				reject({ message: "🤪发布失败，请查看错误信息", err });
+			} else {
+				resolve("🥰发布成功");
+			}
+		}
+	);
+};
+
+// 删除文件
+const deleteFolderRecursive = (path) => {
+	if (fs.existsSync(path)) {
+		fs.readdirSync(path).forEach((file) => {
+			const curPath = path + "/" + file;
+			if (fs.lstatSync(curPath).isDirectory()) {
+				// 递归删除子文件夹
+				deleteFolderRecursive(curPath);
+			} else {
+				// 删除文件
+				fs.unlinkSync(curPath);
+			}
+		});
+
+		// 删除空文件夹
+		fs.rmdirSync(path);
+	}
+};
